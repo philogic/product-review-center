da09fc1198fe323be9330fdd1229f5743230af23
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-12 03:55:32 +0100

    Make model validation tests with test fixtures

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
-class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Customer do
+  let(:anne) {customers(:anne)}
+  let(:peter) {customers(:peter)}
+  let(:margaret) {customers(:margaret)}
+  describe 'validations' do
+    it 'is a valid customer' do
+      expect(anne).must_be :valid?
+    end
+
+    it 'is invalid customer - lack of last name' do
+      expect(peter).must_be :invalid?
+      expect(peter.errors[:last_name]).must_include "can't be blank"
+    end
+
+    it 'is invalid customer - username duplication' do
+      expect(margaret).must_be :invalid?
+      expect(margaret.errors[:username]).must_include 'has already been taken'
+    end
+  end
 end
