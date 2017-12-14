92326eaca536b79fa9b14d9a5336f20b05e8544c
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-14 19:56:03 +0100

    Create home controller with index, new and edit actions with redirecting to devise sign-in/sign-up feature

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  before_action :authenticate_customer!, only: [:index, :new]
+  def index; end
+
+  def new; end
+
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :customers
   root to: "home#index"
 end
