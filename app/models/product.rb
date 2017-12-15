0d6c11f1e356bd92416147c64d314585bdd60550
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-15 02:18:24 +0100

    Create Review and Issue models and migrations and "has many through" association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :reviews
+  has_many :issues
+  has_many :reviewing_customers, through: :reviews, source: :customer
+  has_many :complaining_customers, through: :issues, source: :customer
 end
