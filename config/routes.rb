1ca7a163a53ab7e8db1c2f28d56113949199493f
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-15 01:02:42 +0100

    Adjust devise views and controllers to the project by overriting devise controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  devise_for :customers
+  devise_for :customers, controllers: { registrations: 'registrations' }
   root to: "home#index"
 end
