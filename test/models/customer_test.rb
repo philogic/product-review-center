5e2a325a84debb20613b5ca3361ef5ed8eea9a7b
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-20 08:51:49 +0100

    Create Product and Customer model tests (associations with reviews and issues)

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,6 +4,7 @@ describe Customer do
   let(:anne) {customers(:anne)}
   let(:peter) {customers(:peter)}
   let(:margaret) {customers(:margaret)}
+  let(:paul) {customers(:paul)}
   describe 'validations' do
     it 'is a valid customer' do
       expect(anne).must_be :valid?
@@ -17,6 +18,20 @@ describe Customer do
     it 'is invalid customer - username duplication' do
       expect(margaret).must_be :invalid?
       expect(margaret.errors[:username]).must_include 'has already been taken'
+    end
+  end
+
+  describe 'has reviews' do
+    it 'Anne should have one review' do
+      expect(anne.reviews.size).must_equal 1
+    end
+
+    it 'Margaret should have many reviews' do
+      expect(margaret.reviews.size).must_equal 2
+    end
+
+    it 'Paul should have no reviews' do
+      expect(paul.reviews.size).must_equal 0
     end
   end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Product do
+
+  let(:monopoly) {products(:monopoly)}
+  let(:backgammon) {products(:backgammon)}
+  let(:duke) {products(:duke)}
+  let(:stratego) {products(:stratego)}
+
+  describe 'has reviews' do
+    it 'monopoly should have many reviews' do
+      expect(monopoly.reviews.size).must_equal 2
+    end
+
+    it 'backgammon should have many reviews' do
+      expect(backgammon.reviews.size).must_equal 2
+    end
+
+    it 'duke should have many reviews' do
+      expect(duke.reviews.size).must_equal 1
+    end
+
+    it 'stratego should have no reviews' do
+      expect(stratego.reviews.size).must_equal 0
+    end
+  end
+
+  describe 'has issues' do
+    it 'monopoly should have many issues' do
+      expect(monopoly.issues.size).must_equal 2
+    end
+
+    it 'duke should have no issues' do
+      expect(duke.issues.size).must_equal 0
+    end
+  end
+
+  describe 'has reviewing customers' do
+    it 'monopoly should have many reviewing customers' do
+      expect(monopoly.reviewing_customers.uniq.pluck(:username).size).must_equal 2
+    end
+  end
+
+  describe 'has complaining customers' do
+    it 'monopoly should have exactly one complaining customer' do
+      expect(monopoly.complaining_customers.uniq.pluck(:username).size).must_equal 1
+    end
+  end
 end
