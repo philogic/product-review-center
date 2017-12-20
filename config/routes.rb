c9f24af48f055aaf7a6b6e74b6d06e2286f026f8
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-12-20 09:10:14 +0100

    Add resource route for product and nested resources for issues and reviews. Set default url host in development and test environents.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  devise_for :customers, controllers: { registrations: 'registrations' }
   root to: "home#index"
+  devise_for :customers, controllers: { registrations: 'registrations' }
+  resources :products do
+    resources :issues
+    resources :reviews
+  end
 end
