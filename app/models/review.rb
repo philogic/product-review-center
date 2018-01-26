dcb40cd4d4eaee669600c89e1643ad726a993e83
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-01-26 20:16:35 +0100

    Improve review model in order to generate notifications.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,14 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :comment, length: { minimum: 10 }
   validates :rating, inclusion: { in: (1..5).to_a }
+
+  after_save :broadcast_save
+
+  def broadcast_save
+    ActionCable.server.broadcast 'notifications', status: 'created',
+                                 id: id, product: self.product.name,
+                                 review_author: self.customer.username,
+                                 rating: rating
+
+  end
 end
